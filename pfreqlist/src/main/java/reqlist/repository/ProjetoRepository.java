16edc4ef55f9172ebb52a028fd516457364abfaf
Vinicius da Costa Pires <vinicius.costa.pires@gmail.com>
2014-05-22 00:30:54 -0300

    Adicionando o Spring Data, alterando o Repository e o Service, e colocando um pacote para testes no futuro.

--- pfreqlist/src/main/java/reqlist/repository/ProjetoRepository.java
+++ pfreqlist/src/main/java/reqlist/repository/ProjetoRepository.java
@@ -1,11 +1,7 @@
 package reqlist.repository;
 
 import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
-import org.hibernate.jpa.internal.QueryImpl;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import reqlist.entity.Projeto;
 
@@ -14,14 +10,10 @@ import reqlist.entity.Projeto;
  * @author Vinicius
  */
 @Repository
-public class ProjetoRepository {
+public interface ProjetoRepository {
     
     //@PersistenceContext
     //public EntityManager manager;
-    
-    public List<Projeto> findAll() {
-        //TypedQuery<Projeto> q = manager.createQuery("SELECT p FROM Projeto p", Projeto.class);
-        //return q.getResultList();
-        return null;
-    }
+    @Query("SELECT p FROM Projeto p")
+    public List<Projeto> findAll();
 }
--- pfreqlist/src/main/java/reqlist/service/ProjetoService.java
+++ pfreqlist/src/main/java/reqlist/service/ProjetoService.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package reqlist.service;
 
 import java.util.List;
@@ -23,6 +17,6 @@ public class ProjetoService {
     ProjetoRepository projetoRepository;
     
     public List<Projeto> getProjetos(){
-        return null;
+        return projetoRepository.findAll();
     }
 }
